c87237e7dc0824ad1aabcab467de7f551ee7aba7
luis Ramirez <l.r.32@hotmail.com>
2020-08-06 16:33:12 -0700

    Updates

---
+++ precache-manifest.c9e8f6c8f28854f9484d933744df4d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56c0b0626eb37e6b85803e6456549bd",
+    "url": "/adopt-dogs/index.html"
+  },
+  {
+    "revision": "ea1fe09252b92374a9b9",
+    "url": "/adopt-dogs/static/css/main.5c3571c6.chunk.css"
+  },
+  {
+    "revision": "6ddd9ae5230508ed833a",
+    "url": "/adopt-dogs/static/js/2.fd721208.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/adopt-dogs/static/js/2.fd721208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1fe09252b92374a9b9",
+    "url": "/adopt-dogs/static/js/main.79982975.chunk.js"
+  },
+  {
+    "revision": "1fa23f6f74ff05dcf27f",
+    "url": "/adopt-dogs/static/js/runtime-main.801cf54b.js"
+  }
+]);
